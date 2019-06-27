b0dd9b791846e9ddee3fa8584520ec698782f3fa
Mauro Delazeri <maurodelazeri@gmail.com>
2019-06-26 22:50:58 -0400

    update

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,51 +1,145 @@
-#include <cassert>
-#include <iostream>
-#include <memory>
+// Copyright 2015-2018 The NATS Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-#include "nats/nats.h"
+#include "examples.h"
 
-int count = 0;
-natsStatus error = NATS_OK;
+static const char *usage = ""\
+"-gd            use global message delivery thread pool\n" \
+"-sync          receive synchronously (default is asynchronous)\n" \
+"-count         number of expected messages\n";
 
-void OnError(natsConnection *nc, natsSubscription *sub, natsStatus err,
-             void *closure) {
-  std::cout << "OnError ::: error: " << natsStatus_GetText(err) << std::endl;
-  error = err;
+static void
+onMsg(natsConnection *nc, natsSubscription *sub, natsMsg *msg, void *closure)
+{
+    if (print)
+        printf("Received msg: %s - %.*s\n",
+               natsMsg_GetSubject(msg),
+               natsMsg_GetDataLength(msg),
+               natsMsg_GetData(msg));
+
+    if (start == 0)
+        start = nats_Now();
+
+    // We should be using a mutex to protect those variables since
+    // they are used from the subscription's delivery and the main
+    // threads. For demo purposes, this is fine.
+    if (++count == total)
+        elapsed = nats_Now() - start;
+
+    natsMsg_Destroy(msg);
 }
 
-void OnMessage(natsConnection *nc, natsSubscription *sub, natsMsg *msg,
-               void *closure) {
-  if (msg == nullptr) {
-    std::cout << "OnMessage ::: timeout" << std::endl;
-    return;
-  }
-  std::string strMsg(natsMsg_GetData(msg), natsMsg_GetDataLength(msg));
-  std::cout << "OnMessage ::: subject: " << natsMsg_GetSubject(msg)
-            << ", data_length: " << natsMsg_GetDataLength(msg)
-            << ", data: " << strMsg << std::endl;
-  natsMsg_Destroy(msg);
-  count++;
+static void
+asyncCb(natsConnection *nc, natsSubscription *sub, natsStatus err, void *closure)
+{
+    if (print)
+        printf("Async error: %d - %s\n", err, natsStatus_GetText(err));
+
+    natsSubscription_GetDropped(sub, (int64_t*) &dropped);
 }
 
-int main(int argc, char *argv[]) {
-  natsOptions *opts;
-  assert(natsOptions_Create(&opts) == NATS_OK);
-  std::shared_ptr<natsOptions> opts_releaser(opts, natsOptions_Destroy);
-  assert(natsOptions_SetURL(opts, NATS_DEFAULT_URL) == NATS_OK);
-  assert(natsOptions_SetErrorHandler(opts, OnError, nullptr) == NATS_OK);
+int main(int argc, char **argv)
+{
+    natsConnection      *conn = NULL;
+    natsOptions         *opts  = NULL;
+    natsSubscription    *sub   = NULL;
+    natsStatistics      *stats = NULL;
+    natsMsg             *msg   = NULL;
+    natsStatus          s;
 
-  natsConnection *conn;
-  assert(natsConnection_Connect(&conn, opts) == NATS_OK);
-  std::shared_ptr<natsConnection> conn_releaser(conn, natsConnection_Destroy);
+    opts = parseArgs(argc, argv, usage);
 
-  natsSubscription *sub;
-  assert(natsConnection_Subscribe(&sub, conn, "foo", OnMessage, nullptr) ==
-         NATS_OK);
-  std::shared_ptr<natsSubscription> sub_releaser(sub, natsSubscription_Destroy);
+    printf("Listening %ssynchronously on '%s'.\n",
+           (async ? "a" : ""), subj);
 
-  while (error == NATS_OK) {
-      break;
-  }
+    s = natsOptions_SetErrorHandler(opts, asyncCb, NULL);
 
-  return 0;
+    if (s == NATS_OK)
+        s = natsConnection_Connect(&conn, opts);
+
+    if (s == NATS_OK)
+    {
+        if (async)
+            s = natsConnection_Subscribe(&sub, conn, subj, onMsg, NULL);
+        else
+            s = natsConnection_SubscribeSync(&sub, conn, subj);
+    }
+
+    // For maximum performance, set no limit on the number of pending messages.
+    if (s == NATS_OK)
+        s = natsSubscription_SetPendingLimits(sub, -1, -1);
+
+    if (s == NATS_OK)
+        s = natsSubscription_AutoUnsubscribe(sub, (int) total);
+
+    if (s == NATS_OK)
+        s = natsStatistics_Create(&stats);
+
+    if ((s == NATS_OK) && async)
+    {
+        while (s == NATS_OK)
+        {
+            s = printStats(STATS_IN|STATS_COUNT, conn, sub, stats);
+
+            if (count + dropped == total)
+                break;
+
+            if (s == NATS_OK)
+                nats_Sleep(1000);
+        }
+    }
+    else if (s == NATS_OK)
+    {
+        int64_t last = 0;
+
+        for (count = 0; (s == NATS_OK) && (count < total); count++)
+        {
+            s = natsSubscription_NextMsg(&msg, sub, 10000);
+            if (s != NATS_OK)
+                break;
+
+            if (start == 0)
+                start = nats_Now();
+
+            if (nats_Now() - last >= 1000)
+            {
+                s = printStats(STATS_IN|STATS_COUNT, conn, sub, stats);
+                last = nats_Now();
+            }
+
+            natsMsg_Destroy(msg);
+        }
+    }
+
+    if (s == NATS_OK)
+    {
+        printStats(STATS_IN|STATS_COUNT,conn, sub, stats);
+        printPerf("Received", count, start, elapsed);
+    }
+    else
+    {
+        printf("Error: %d - %s\n", s, natsStatus_GetText(s));
+        nats_PrintLastErrorStack(stderr);
+    }
+
+    // Destroy all our objects to avoid report of memory leak
+    natsStatistics_Destroy(stats);
+    natsSubscription_Destroy(sub);
+    natsConnection_Destroy(conn);
+    natsOptions_Destroy(opts);
+
+    // To silence reports of memory still in used with valgrind
+    nats_Close();
+
+    return 0;
 }
